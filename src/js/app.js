f06cfac981b593f83e24ff36cf8aa0dd3c3a0f64
Aakash Rastogi <1998aakashrastogi@gmail.com>
2017-11-26 03:35:05 +0530

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,14 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('https://aakashrstg00.github.io/testpwa/sw.js').then(() => {
+    navigator.serviceWorker.register('../../sw.js').then(() => {
         console.log('service worker registered!');
     });
 }
 
+var deferredPrompt;
+
 window.addEventListener('beforeinstallprompt', (e) => {
     console.log('before install prompt fired');
     e.preventDefault();
+    deferredPrompt = e;
+    return false;
 })
