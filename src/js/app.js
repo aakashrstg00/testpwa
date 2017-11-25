3df059ba3efe2d7d8fa065890bc22edae81dd124
Aakash Rastogi <1998aakashrastogi@gmail.com>
2017-11-26 03:39:12 +0530

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -12,4 +12,33 @@ window.addEventListener('beforeinstallprompt', (e) => {
     deferredPrompt = e;
     return false;
 });
-module.exports = deferredPrompt;
+
+var shareImageButton = document.querySelector('#share-image-button');
+var createPostArea = document.querySelector('#create-post');
+var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+var deferredPrompt = require('./app.js').deferredPrompt;
+
+function openCreatePostModal() {
+    createPostArea.style.display = 'block';
+    if (deferredPrompt) {
+        deferredPrompt.prompt();
+
+        deferredPrompt.userChoice.then((result) => {
+            console.log(result.outcome);
+            if (result.outcome === 'dismissed') {
+                console.log('installation cancelled');
+            } else {
+                console.log('install success');
+            }
+        });
+        deferredPrompt = null;
+    }
+}
+
+function closeCreatePostModal() {
+    createPostArea.style.display = 'none';
+}
+
+shareImageButton.addEventListener('click', openCreatePostModal);
+
+closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
